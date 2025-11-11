80efc403248b6950f3fe4ab27ca9c95dc7662411
tunguyenhoang1096 <tunguyenhoang1096@gmail.com>
2025-11-12 07:28:59 +0900

    Add files via upload

---
+++ ex3/main.js
@@ -0,0 +1,9 @@
+// hiển thị 20 số nguyên tố đầu tiên
+// số nguyên tố là số chia hết cho 1 và chính nó
+/*  tạo 1 biến đếm đến 20 cho vòng lặp while
+    tạo 1 biến num để kiểm tra có phải số nguyên tố không
+    tạo 1 biến result để trả về kết quả */
+let count = 0;
+let num,
+  result = "";
+while (count < 20) {}
