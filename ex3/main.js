073a3bc916e196cd809076cb9ce4f9be4ed076a3
tunguyenhoang1096 <tunguyenhoang1096@gmail.com>
2025-11-20 20:51:47 +0900

    Add files via upload

--- ex3/main.js
+++ ex3/main.js
@@ -3,7 +3,24 @@
 /*  tạo 1 biến đếm đến 20 cho vòng lặp while
     tạo 1 biến num để kiểm tra có phải số nguyên tố không
     tạo 1 biến result để trả về kết quả */
+// let count = 0;
+// let num,
+//   result = "";
+// while (count < 20) {}
+
 let count = 0;
-let num,
-  result = "";
-while (count < 20) {}
+let num = 2;
+while (count < 20) {
+  let isPrime = true;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    console.log(num);
+    count++;
+  }
+  num++;
+}
